c5068ff52030d34ee596763907924b0a3d0027ec
root <root@iZ2ze3uk97m2bfv681kyx4Z.(none)>
2017-10-09 22:12:25 +0800

    nginx: config.sh

---
+++ config.sh
@@ -0,0 +1,4 @@
+#./auto/configure --add-module=/home/share/nginx/private/ngx_http_hello_world_module
+#./auto/configure --add-module=/home/share/nginx/private//ngx_http_mytest_module
+./auto/configure --add-module=/home/share/nginx/private//ngx_http_myfilter_module --add-module=/home/share/nginx/private/ngx_http_hello_world_module
+#./configure --add-module="/mnt/hgfs/share_ubuntu/git_nginx/nginx-1.10.3/sgy/ngx_http_hello_world_module /mnt/hgfs/share_ubuntu/git_nginx/nginx-1.10.3/sgy/ngx_http_myfilter_module"
