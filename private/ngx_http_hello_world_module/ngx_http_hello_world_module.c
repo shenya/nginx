817f830666e80e117c8e36735dbe009fe6a232cb
root <root@iZ2ze3uk97m2bfv681kyx4Z.(none)>
2017-10-14 12:34:45 +0800

    Process request from client with json.

--- private/ngx_http_hello_world_module/ngx_http_hello_world_module.c
+++ private/ngx_http_hello_world_module/ngx_http_hello_world_module.c
@@ -1,10 +1,12 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 #include <ngx_http.h>
+#include <pri_test.h>
+#include "cJSON.h"
 
 typedef struct {
     ngx_str_t output_words;
-    ngx_flag_t flag;
+    ngx_uint_t flag;
 } ngx_http_hello_world_loc_conf_t;
 
 // To process HelloWorld command arguments
@@ -66,19 +68,143 @@ ngx_module_t ngx_http_hello_world_module = {
     NGX_MODULE_V1_PADDING
 };
 
-#if 1
-static ngx_int_t ngx_http_hello_world_handler(ngx_http_request_t* r) {
-    ngx_int_t rc;
+static void ngx_http_hello_world_client_body_handler_pt(ngx_http_request_t *r)
+{
+    ngx_int_t rc = NGX_OK;
     ngx_buf_t* b;
     ngx_chain_t out[2];
+    char log_buf[32] = {0};
+    u_char body_buf[256] = {0};
+    ngx_buf_t *p_body_buf = NULL;
+    cJSON *root = NULL;
+    cJSON *name = NULL;
 
-    ngx_http_hello_world_loc_conf_t* hlcf;
+    ngx_http_hello_world_loc_conf_t* hlcf = NULL;
     hlcf = ngx_http_get_module_loc_conf(r, ngx_http_hello_world_module);
 
+    show_str(log_buf, 32);
+    ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0,
+                          "%s", log_buf);
+
+    p_body_buf = r->request_body->bufs->buf;
+    ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0,
+                          "length:%d", ngx_buf_size(p_body_buf));
+    
+    ngx_snprintf(body_buf, sizeof(body_buf), "%*s",
+            ngx_buf_size(p_body_buf), p_body_buf->pos);
+
+    ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0,
+            "receive body:%s", body_buf);
+
+    root = cJSON_Parse((char *)body_buf);
+    if (NULL == root)
+    {
+
+    }
+    name = cJSON_GetObjectItemCaseSensitive(root, "name");
+
+    ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0,
+                          "name: %s", name->valuestring);
+#if 1
+        r->headers_out.content_type.len = sizeof("text/plain") - 1;
+        r->headers_out.content_type.data = (u_char*)"text/plain";
+    
+        b = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
+    
+        //
+        show_str(log_buf, 32);
+        ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0,
+                              "%s", log_buf);
+    
+        ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0,
+                              "uri:%V, args: %V", &r->uri,
+                              &r->args);
+    
+        out[0].buf = b;
+        out[0].next = &out[1];
+    
+        if (1 == hlcf->flag)
+        {
+            b->pos = (u_char*)"hello_world1, ";
+            b->last = b->pos + sizeof("hello_world1, ") - 1;
+        }
+        else
+        {
+            b->pos = (u_char*)"hello_world2, ";
+            b->last = b->pos + sizeof("hello_world2, ") - 1;
+        }
+    
+        b->memory = 1;
+    
+        b = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
+    
+        out[1].buf = b;
+        out[1].next = NULL;
+    
+        b->pos = hlcf->output_words.data;
+        b->last = hlcf->output_words.data + (hlcf->output_words.len);
+        b->memory = 1;
+        b->last_buf = 1;
+    
+        r->headers_out.status = NGX_HTTP_OK;
+        r->headers_out.content_length_n = hlcf->output_words.len + sizeof("hello_world1, ") - 1;
+        rc = ngx_http_send_header(r);
+        if (rc == NGX_ERROR || rc > NGX_OK || r->header_only) {
+            return ;
+        }
+    
+        rc = ngx_http_output_filter(r, &out[0]);
+        ngx_http_finalize_request(r,rc);
+
+        return;
+#endif
+}
+#if 1
+static ngx_int_t ngx_http_hello_world_handler(ngx_http_request_t* r) {
+    ngx_int_t rc = NGX_OK;
+    //ngx_buf_t* b;
+    //ngx_chain_t out[2];
+    //char log_buf[32] = {0};
+
+    //ngx_http_hello_world_loc_conf_t* hlcf = NULL;
+    //hlcf = ngx_http_get_module_loc_conf(r, ngx_http_hello_world_module);
+
+    if (ngx_atoi(r->headers_in.content_length->value.data, r->headers_in.content_length->value.len) <= 0)
+    {
+        ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0,
+                          "%s", "content length is 0");
+        return rc;
+    }
+
+    ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0,
+                          "content length:%d", ngx_atoi(r->headers_in.content_length->value.data, r->headers_in.content_length->value.len));
+
+    rc = ngx_http_read_client_request_body(r,
+            ngx_http_hello_world_client_body_handler_pt);
+
+    if (rc >= NGX_HTTP_SPECIAL_RESPONSE)
+    {
+        ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0,
+                          "%s", "unknown respone");
+        return rc;
+    }
+
+    return NGX_DONE;
+
+#if 0
     r->headers_out.content_type.len = sizeof("text/plain") - 1;
     r->headers_out.content_type.data = (u_char*)"text/plain";
 
     b = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
+
+    //
+    show_str(log_buf, 32);
+    ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0,
+                          "%s", log_buf);
+
+    ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0,
+                          "uri:%V, args: %V", &r->uri,
+                          &r->args);
 
     out[0].buf = b;
     out[0].next = &out[1];
@@ -114,6 +240,7 @@ static ngx_int_t ngx_http_hello_world_handler(ngx_http_request_t* r) {
     }
 
     return ngx_http_output_filter(r, &out[0]);
+#endif
 }
 
 #else
@@ -190,7 +317,8 @@ static char* ngx_http_hello_world_merge_loc_conf(ngx_conf_t* cf, void* parent, v
     ngx_http_hello_world_loc_conf_t* prev = parent;
     ngx_http_hello_world_loc_conf_t* conf = child;
     ngx_conf_merge_str_value(conf->output_words, prev->output_words, "Nginx");
-    //ngx_conf_merge_uint_value(conf->flag, prev->flag, 0);
+    ngx_conf_merge_uint_value(conf->flag, prev->flag,
+            1);
     return NGX_CONF_OK;
 }
 
